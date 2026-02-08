9e1785747e6da14eec061f1cac4fc5a8cab18d8d
Walter Folini <walter@accountable.eu>
2026-02-08 15:51:55 +0100

    refactor: reorganize hooks with kebab-case naming
    - Rename hooks files to kebab-case convention
    - Update imports in components and screens

---
+++ src/hooks/coins/use-coins-list.ts
@@ -0,0 +1,39 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+
+import { coinsListWithMarketData } from "@/api/coins";
+
+export function useCoinsList({
+	coinIds,
+	perPage = 10,
+}: {
+	coinIds: string | null | undefined;
+	perPage?: number;
+}) {
+	// coinIds = null -> Fetch all coins
+	// coinIds = "someid" -> Fetch coins with id "someid"
+	// coinIds = "" || undefined -> Query with no results. Don't fetch anything, returns empty array
+	const validQuery = !(coinIds === "" || coinIds === undefined);
+
+	const { data, ...queryResult } = useInfiniteQuery({
+		queryKey: ["coinsList", coinIds, perPage],
+		queryFn: ({ pageParam }) =>
+			coinsListWithMarketData({
+				page: pageParam,
+				ids: coinIds || undefined,
+				perPage,
+			}),
+		getNextPageParam: (lastPage) =>
+			lastPage.data.length === perPage
+				? lastPage.config.params.page + 1
+				: undefined,
+		initialPageParam: 1,
+		select: (data) =>
+			validQuery ? data?.pages.flatMap((page) => page.data) : [],
+		enabled: validQuery,
+	});
+
+	return {
+		coins: data,
+		...queryResult,
+	};
+}
