629ad1bc9fa6594e5e106ccef6aa72f4daaa8732
Walter Folini <walter@accountable.eu>
2026-02-08 16:27:05 +0100

    refactor: rename CoinSearchScreen to kebab-case file
    - Rename CoinSearchScreen.tsx to coin-search.tsx
    - Update Tabs.Screen name to "coin-search"
    - Keep component export as CoinSearchScreen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -27,7 +27,7 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="CoinSearchScreen"
+        name="coin-search"
         options={{
           title: "Market",
           tabBarIcon: ({ color, size }) => (
